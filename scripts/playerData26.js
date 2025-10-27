7b0d7675846b8756bf85224d849269fe0f5e36f7
tylerhealyy <tylerhealy516@gmail.com>
2025-10-26 22:03:43 -0400

    bug

---
+++ scripts/playerData26.js
@@ -0,0 +1,30 @@
+export const playerData26 = [
+  {
+    name: "Drew Allar",
+    position: "QB",
+    school: "Penn State",
+    schoolLogo: "https://upload.wikimedia.org/wikipedia/en/thumb/3/3a/Penn_State_Nittany_Lions_logo.svg/1200px-Penn_State_Nittany_Lions_logo.svg.png",
+    height: "6'5",
+    weight: "236",
+    age: "21",
+    rank: 1,
+    positionRank: 1,
+    consensus: 1,
+    analysis: "(4/30): Pocket passer with a strong arm and inconsistent accuracy. Shows flashes of strong processing and full-field read ability. Elite pocket presence and sack avoidance, but doesn't usually find throws after escaping. Dropback mechanics need work. Okay-at-best athleticism.",
+    stats: "https://www.sports-reference.com/cfb/players/drew-allar-1.html"
+  },
+  {
+    name: "Garrett Nussmeier",
+    position: "QB",
+    school: "LSU",
+    schoolLogo: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4a/LSU_Athletics_logo.svg/2560px-LSU_Athletics_logo.svg.png",
+    height: "6'2",
+    weight: "200",
+    age: "23",
+    rank: 2,
+    positionRank: 2,
+    consensus: 2,
+    analysis: "(5/2): Inconsistent passer with no elite traits. High velocity throws but lacking accuracy, poor downfield arm strength. Shows flashes of tight window accuracy and dropping deep balls in the bucket, but very inconsistent. Sloppy pocket work, bails early very often, resulting in bad timing. Fades away on throws, especially under pressure. Throws on the run often, can be accurate. Not much to like in his processing, rarely shows proper progressions and often predetermines and forces throws/makes bad decisions. Almost never runs past LOS.",
+    stats: "https://www.sports-reference.com/cfb/players/garrett-nussmeier-1.html"
+  }
+]
